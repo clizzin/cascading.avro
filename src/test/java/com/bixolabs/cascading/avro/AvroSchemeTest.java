32c386b62d2b5f4656e81f303e76d062895ba23e
Ken Krugler <kkrugler@transpac.com>
2010-04-15 12:26:37 -0700

    Get rid of integration tests.

--- src/test/java/com/bixolabs/cascading/avro/AvroSchemeTest.java
+++ src/test/java/com/bixolabs/cascading/avro/AvroSchemeTest.java
@@ -1,6 +1,10 @@
 package com.bixolabs.cascading.avro;
 
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
+
+import cascading.tuple.Fields;
 
 public class AvroSchemeTest {
 
@@ -8,4 +12,33 @@ public class AvroSchemeTest {
     public void testAvroSchemaGeneration() {
         // TODO
     }
+    
+    @Test
+    public void testSchemeChecks() {
+        try {
+            new AvroScheme(new Fields(), new Class[0]);
+            fail("Exception should be thrown when scheme field is empty");
+        } catch (Exception e) {
+            // valid
+        }
+
+        try {
+            new AvroScheme(new Fields("a", "b", "c"), new Class[0]);
+            fail("Exception should be thrown when there isn't one class per field");
+        } catch (Exception e) {
+            // valid
+        }
+
+        try {
+            new AvroScheme(new Fields("a"), new Class[] { Integer.class });
+        } catch (Exception e) {
+            fail("Exception should not be thrown");
+        }
+    }
+
+    @Test
+    public void testRoundTrip() throws Exception {
+        
+    }
+
 }
